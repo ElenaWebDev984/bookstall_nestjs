5c52677986ea1696834cf7f2ac2a1c4d5a56599c
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-04 07:49:58 +0200

    added CRUD for controller

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  // Получить список всех книг
+  @Get()
+  async getAllBooks() {
+    // необходимо вызвать соответствующий метод сервиса и вернуть результат
+    //const result = await this.booksService.someMethod();
+    //return result
+  }
+
+  // Получить книгу по ID
+  @Get(':id')
+  async getBookById(@Param('id') id: number) {
+    // необходимо вызвать соответствующий метод сервиса и вернуть результат
+    //const result = await this.booksService.someMethod();
+    //return result
+  }
+
+  // Создать новую книгу
+  @Post()
+  async createBook(@Body() bookDto: any) {
+    // необходимо вызвать соответствующий метод сервиса и вернуть результат
+    //const result = await this.booksService.someMethod();
+  }
+
+  // Обновить информацию о книге
+  @Put(':id')
+  async updateBook(@Param('id') id: number, @Body() bookDto: any) {
+    // необходимо вызвать соответствующий метод сервиса и вернуть результат
+    //const result = await this.booksService.someMethod();
+  }
+
+  // Удалить книгу
+  @Delete(':id')
+  async deleteBook(@Param('id') id: number) {
+    // необходимо вызвать соответствующий метод сервиса и вернуть результат
+    //const result = await this.booksService.someMethod();
+  }
+}
