9f2331da55647c59a7c9271bc4d985c4ef95bd4d
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-12 10:21:25 +0200

    added import JWT module for AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'secret_key', // секретный ключ (должен браться из env)
+      signOptions: { expiresIn: '60m' }, // Время жизни токена
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
