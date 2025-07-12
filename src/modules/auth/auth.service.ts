64919ed5ac901dc394da0689e301d2aebdd5ccaa
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-12 10:52:44 +0200

    added validateUser

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersRepository } from '../users/users.repository';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<{ userId: number }> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('message');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('message');
+    }
+
+    return {
+      userId: user.id,
+    };
+  }
+}
