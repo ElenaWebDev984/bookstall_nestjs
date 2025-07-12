ae0c045bf0cbee66864de8390ae3a5fae55abc87
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-12 11:19:35 +0200

    created login

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from '../users/users.repository';
 import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(
     email: string,
@@ -24,6 +28,14 @@ export class AuthService {
 
     return {
       userId: user.id,
+    };
+  }
+
+  login(userId: string) {
+    const token = this.jwtService.sign(userId);
+
+    return {
+      accessToken: token,
     };
   }
 }
