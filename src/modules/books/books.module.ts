2e2667d486990cf7477b1503e1071b9d5d089702
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-20 19:35:53 +0200

    added  UsersModule

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -5,9 +5,10 @@ import { BooksController } from './books.controller';
 import { BooksRepository } from './books.repository';
 import { Book } from './book.entity';
 import { JwtStrategy } from '../../core/guards/jwt.strategy';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [TypeOrmModule.forFeature([Book]), UsersModule],
   controllers: [BooksController],
   providers: [BooksService, BooksRepository, JwtStrategy],
 })
