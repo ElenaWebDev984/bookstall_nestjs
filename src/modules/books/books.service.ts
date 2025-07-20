cfcbcc1195ba48d12b68c685eaa42dcebc2fc5ee
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-20 19:36:50 +0200

    added  userRepo and updateBook

--- src/modules/books/books.service.ts
+++ src/modules/books/books.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { BooksRepository } from './books.repository';
 import { Book } from './book.entity';
 import { CreateBookDto } from './dto/create-book.dto';
+import { UsersRepository } from '../users/users.repository';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
 export class BooksService {
-  constructor(private readonly booksRepository: BooksRepository) {}
+  constructor(
+    private readonly booksRepository: BooksRepository,
+    private userRepo: UsersRepository,
+  ) {}
 
   // Получить список всех книг
   async getAllBooks(): Promise<Book[]> {
@@ -18,12 +23,17 @@ export class BooksService {
   }
 
   // Создать новую книгу
-  async createBook(dto: CreateBookDto): Promise<void> {
-    const book = new Book();
-    book.title = dto.title;
-    book.ageRestriction = dto.ageRestriction;
-    book.author = dto.author;
+  async createBook(dto: CreateBookDto, userId: number): Promise<void> {
+    const user = await this.userRepo.findByIdOrNotFoundFail(userId);
+
+    const book = Book.createBook(dto, userId, user.age);
 
     await this.booksRepository.save(book);
+  }
+
+  async updateBook(dto: UpdateBookDto, userId: number, bookId: number) {
+    const book = await this.booksRepository.findOneOrNotFoundFail(bookId);
+
+    book.updateBook(dto, userId);
   }
 }
