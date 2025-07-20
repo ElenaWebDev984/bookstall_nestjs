6af34afb7beb78b0e372e5bdf0ae3534222f1b2e
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-20 19:34:40 +0200

    added createBokk and updateBook

--- src/modules/books/book.entity.ts
+++ src/modules/books/book.entity.ts
@@ -1,5 +1,8 @@
 import { Entity, Column } from 'typeorm';
 import { BaseEntity } from '../../core/entity/base.entity';
+import { CreateBookDto } from './dto/create-book.dto';
+import { ForbiddenException } from '@nestjs/common';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Entity('books')
 export class Book extends BaseEntity {
@@ -17,4 +20,26 @@ export class Book extends BaseEntity {
 
   @Column({ nullable: true })
   image?: string;
+
+  static createBook(dto: CreateBookDto, userId: number, userAge: number): Book {
+    if (userAge < 18 && dto.ageRestriction >= 18) {
+      throw new ForbiddenException('too yang, Bro');
+    }
+
+    const book = new Book();
+    book.title = dto.title;
+    book.ageRestriction = dto.ageRestriction;
+    book.author = dto.author;
+    book.ownerId = userId;
+
+    return book;
+  }
+  updateBook(dto: UpdateBookDto, userId: number) {
+    // TODO logic
+    if (this.ownerId !== userId) {
+      throw new ForbiddenException();
+    }
+
+    this.title = dto.title ?? this.title;
+  }
 }
