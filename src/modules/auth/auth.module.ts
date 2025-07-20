108a9e75f77293e96e63818dfc2fe056088ddfbf
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-20 15:12:35 +0200

    added import for LocalStrategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [
@@ -15,6 +16,6 @@ import { UsersModule } from '../users/users.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
