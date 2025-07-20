474b2547ab03514edbc7815ad3f9aecc6c90bc12
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-20 15:43:23 +0200

    added import for  JwtAuthGuard

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -4,10 +4,11 @@ import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { BooksRepository } from './books.repository';
 import { Book } from './book.entity';
+import { JwtStrategy } from '../../core/guards/jwt.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book])],
   controllers: [BooksController],
-  providers: [BooksService, BooksRepository],
+  providers: [BooksService, BooksRepository, JwtStrategy],
 })
 export class BooksModule {}
